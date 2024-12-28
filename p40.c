934fd71c538caec54cbc152c30ed3331b1f0b67d
unknown <ujwalsgowda2006@gmail.com>
2024-12-28 14:57:22 +0530

    assignments

---
+++ p40.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    int arr[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    
+    printf("[ "); 
+    for (int i = 0; i < 2; i++) { 
+        for (int j = 0; j < 3; j++) { 
+            printf("%d", arr[i][j]);
+            if (j < 2) printf(" "); 
+        }
+        if (i < 1) printf("\n  "); 
+    }
+    printf(" ]\n"); 
+    return 0;
+}
+
