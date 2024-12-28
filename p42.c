5fa43bb746bc7740631aa0ad0f93a766fdd4d472
unknown <ujwalsgowda2006@gmail.com>
2024-12-28 21:38:07 +0530

    assignments

---
+++ p42.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello, World!";
+    int length = strlen(str);
+
+    printf("The length of the string \"%s\" is %d.\n", str, length);
+    return 0;
+}
